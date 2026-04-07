d42b65a5627ac987c1d1322f449e2071b901de99
zcus <zcusgt@gmail.com>
2026-04-07 23:16:30 +0700

    auto build: Release

--- C#/AutoMinigameWinForms/AppConstants.cs
+++ C#/AutoMinigameWinForms/AppConstants.cs
@@ -18,9 +18,9 @@ public static class AppConstants
     public const bool AutoPressUseOcrKey = true;
     public const double PressDelaySec = 0.07;
     public const int PressRequireStableFrames = 1;
-    public const double PressStrictMaxDiffDeg = 9.0;
+    public const double PressStrictMaxDiffDeg = 6.2;
     public const double PressStrictTolRatio = 0.70;
-    public const int PressRearmClearFrames = 1;
+    public const int PressRearmClearFrames = 2;
 
     public const int OcrBoxW = 40;
     public const int OcrBoxH = 40;
